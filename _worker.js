32588c5b189fc9804884b98f88d9f7b7ddd6954a
Atrexe <96480429+Atrexe@users.noreply.github.com>
2024-06-18 16:06:10 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,8 +18,7 @@ let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://raw.githubusercontent.com/ansoncloud8/am-tunnel/dev/ipv4.txt', //可参考内容格式 自行搭建。
-	//'https://raw.githubusercontent.com/ansoncloud8/am-tunnel/dev/ipv6.txt', //IPv6优选内容格式 自行搭建。
+	'https://raw.githubusercontent.com/Atrexe/CF-Workers-Pages/main/ipv4.txt', //可参考内容格式 自行搭建。
 ];
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
